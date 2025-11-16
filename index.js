7cabca29e3441fb11a9a6ce679860decc2d0c5ee
lilahgit <sitikholilah@hotmail.com>
2025-11-16 18:44:56 +0700

    user input handling done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code here, goodluck!!
+'use strict';
+
+// 1. User Input Handling
+let prompt = require("prompt-sync")();
+
+function getValidNumberInput (prompMessage){
+    let num;
+    let flag = 0;
+    do {
+        flag++;
+        if (flag>1)
+         console.log("Invalid input. Please enter a valid number.");
+        num = prompt(prompMessage);
+    } while (isNaN(num) || num.trim() === ''); 
+    return parseFloat(num);
+}
+
+function getValidOperatorInput (prompMessage){
+    let num;
+    let flag = 0;
+    do {
+        flag++;
+        if (flag>1)
+         console.log("Invalid operator. Please enter one of +, -, *, /, %, **");
+        num = prompt(prompMessage);
+    } while (isNaN(num) || num.trim() === ''); 
+    return parseFloat(num);
+}
