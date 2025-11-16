12353285b626d3f83cb47fc0f1d95823db4be2cd
lilahgit <sitikholilah@hotmail.com>
2025-11-16 19:03:29 +0700

    basic arithmetic operation added

--- index.js
+++ index.js
@@ -16,13 +16,35 @@ function getValidNumberInput (prompMessage){
 }
 
 function getValidOperatorInput (prompMessage){
-    let num;
-    let flag = 0;
-    do {
-        flag++;
-        if (flag>1)
-         console.log("Invalid operator. Please enter one of +, -, *, /, %, **");
-        num = prompt(prompMessage);
-    } while (isNaN(num) || num.trim() === ''); 
-    return parseFloat(num);
-}
+    const validOperators = ['+', '-', '*', '/', '%', '**'];
+    let operator;
+    if (validOperators.includes(operator)) {
+            return operator;
+        }
+        console.log("Invalid operator. Please enter one of +, -, *, /, %, **");
+    }
+
+// 2. Basic Arithmetic Operation (Functions and Operators)
+function add(a, b) {
+    return a + b;
+    }
+
+    function subtract(a, b) {
+        return a - b;
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+    function divide(a, b) {
+        return b === 0 ? "Error: Division by zero!" : a / b;
+    }
+
+    function modulo(a, b) {
+        return b === 0 ? "Error: Division by zero!" : a % b;
+    }
+
+    function power(a, b) {
+        return a ** b;
+    }
